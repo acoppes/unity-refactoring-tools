c6bbea923dc65737fc080324845221a4e95cd41e
Ariel Coppes <ariel.coppes@gmail.com>
2022-04-23 21:32:44 -0300

    Added example for prefab and scenes

---
+++ Assets/CustomBehaviour.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBehaviour : MonoBehaviour
+{
+    public int previousValue;
+    public string newValue;
+}
