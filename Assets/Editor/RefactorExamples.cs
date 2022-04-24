31d3d1d7c90c3b9d31e067122e7dd1de6a09178c
Ariel Coppes <ariel.coppes@gmail.com>
2022-04-23 21:29:45 -0300

    Moved to separated class

--- Assets/Editor/RefactorExamples.cs
+++ Assets/Editor/RefactorExamples.cs
@@ -1,50 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Utils.Editor;
 
 public static class RefactorExamples
 {
-    public static void RefactorAsset<T>(Func<T, bool> callback) where T : UnityEngine.Object
-    {
-        var guids = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(T)}", null);
-        var assets = guids.Select(g => UnityEditor.AssetDatabase.LoadAssetAtPath<T>(
-            UnityEditor.AssetDatabase.GUIDToAssetPath(g))).ToList();
-        
-        try
-        {
-            var total = assets.Count;
-
-            UnityEditor.EditorUtility.DisplayProgressBar($"Refactoring {total} assets of type {typeof(T).Name}", "Start", 0);
-
-            for (var i = 0; i < assets.Count; i++)
-            {
-                var asset = assets[i];
-                UnityEditor.EditorUtility.DisplayProgressBar($"Refactoring {assets.Count} assets of type {typeof(T).Name}",
-                    asset.name,
-                    i / (float)total);
-
-                var result = callback(asset);
-                // Just to break the loop if something is wrong...
-                if (!result)
-                {
-                    break;
-                }
-                
-                UnityEditor.EditorUtility.SetDirty(asset);
-            }
-            
-            UnityEditor.AssetDatabase.SaveAssets();
-        }
-        finally
-        {
-            UnityEditor.EditorUtility.ClearProgressBar();
-        }
-    }
+   
     
     [UnityEditor.MenuItem("Refactors/Refactor Custom Data")]
     public static void RefactorCustomData()
     {
-        RefactorAsset(delegate(CustomDataAsset asset)
+        RefactorTools.RefactorAsset(delegate(CustomDataAsset asset)
         {
             asset.newValue = $"VALUE:{asset.previousValue}";
             return true;
---
+++ Assets/Editor/RefactorTools.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+
+namespace Utils.Editor
+{
+    public static class RefactorTools
+    {
+        public static void RefactorAsset<T>(Func<T, bool> callback) where T : UnityEngine.Object
+        {
+            var guids = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(T)}", null);
+            var assets = guids.Select(g => UnityEditor.AssetDatabase.LoadAssetAtPath<T>(
+                UnityEditor.AssetDatabase.GUIDToAssetPath(g))).ToList();
+        
+            try
+            {
+                var total = assets.Count;
+
+                UnityEditor.EditorUtility.DisplayProgressBar($"Refactoring {total} assets of type {typeof(T).Name}", "Start", 0);
+
+                for (var i = 0; i < assets.Count; i++)
+                {
+                    var asset = assets[i];
+                    UnityEditor.EditorUtility.DisplayProgressBar($"Refactoring {assets.Count} assets of type {typeof(T).Name}",
+                        asset.name,
+                        i / (float)total);
+
+                    var result = callback(asset);
+                    // Just to break the loop if something is wrong...
+                    if (!result)
+                    {
+                        break;
+                    }
+                
+                    UnityEditor.EditorUtility.SetDirty(asset);
+                }
+            
+                UnityEditor.AssetDatabase.SaveAssets();
+            }
+            finally
+            {
+                UnityEditor.EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+}
